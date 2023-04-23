86f6386614d7b5f4a2af3a87c07241bd5e391d94
BaoCaiH <caihongbao280996@gmail.com>
2023-04-23 22:59:32 +0200

    move push action to controller

--- sokoban/Assets/Scripts/PlayerPush.cs
+++ sokoban/Assets/Scripts/PlayerPush.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerPush : MonoBehaviour
 {
-    [SerializeField] private float checkRadius = 0.45f;
-    [SerializeField] private Transform checkFront;
-    [SerializeField] private LayerMask crate;
+    //[SerializeField] private float checkRadius = 0.45f;
+    //[SerializeField] private Transform checkFront;
+    //[SerializeField] private LayerMask crate;
 
-    private float pushHorizontal;
-    private float pushVertical;
-    private Collider2D foundCrate;
+    //private float pushHorizontal;
+    //private float pushVertical;
+    //private Collider2D foundCrate;
 
     // Start is called before the first frame update
     void Start()
@@ -21,21 +21,21 @@ public class PlayerPush : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pushVertical = Input.GetAxisRaw("Vertical");
-        pushHorizontal = Input.GetAxisRaw("Horizontal");
-        Vector2 direction = pushVertical > 0f ? Vector2.up
-            : pushVertical < 0f ? Vector2.down
-            : pushHorizontal > 0f ? Vector2.right
-            : pushHorizontal < 0f ? Vector2.left
-            : Vector2.zero;
-        foundCrate = null;
-        if (direction != Vector2.zero)
-        {
-            foundCrate = Physics2D.OverlapCircle((Vector2)transform.position + direction / 2, checkRadius, crate);
-        }
-        if (foundCrate is not null)
-        {
-            foundCrate.gameObject.GetComponent<CrateMove>().Push(direction);
-        }
+        //pushVertical = Input.GetAxisRaw("Vertical");
+        //pushHorizontal = Input.GetAxisRaw("Horizontal");
+        //Vector2 direction = pushVertical > 0f ? Vector2.up
+        //    : pushVertical < 0f ? Vector2.down
+        //    : pushHorizontal > 0f ? Vector2.right
+        //    : pushHorizontal < 0f ? Vector2.left
+        //    : Vector2.zero;
+        //foundCrate = null;
+        //if (direction != Vector2.zero)
+        //{
+        //    foundCrate = Physics2D.OverlapCircle((Vector2)transform.position + direction / 2, checkRadius, crate);
+        //}
+        //if (foundCrate is not null)
+        //{
+        //    foundCrate.gameObject.GetComponent<CrateMove>().Push(direction);
+        //}
     }
 }
